4d7090c640092c7ebaddadd0622fe60ae0e3c515
lticdiyk <lticdiyk@gmail.com>
2023-03-27 02:06:47 +0900

    Initial commit

---
+++ src/main/webapp/sample01/JS/myjs.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+ 
+function btnFunc() {
+	//alert("btnFunc() 호출");
+	
+	var demo = document.getElementById("demo").innerHTML;	
+	document.getElementById("data").innerHTML = demo;
+}
+
